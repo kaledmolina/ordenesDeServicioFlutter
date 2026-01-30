3005ab0a00a0366b6ee0f5396d08e6be07cde272
kaledmolina <kaledmoly@gmail.com>
2026-01-30 09:37:18 -0500

    feat: add path, url, status, and errorMessage fields to the PhotoDisplay model.

--- lib/models/photo_status_model.dart
+++ lib/models/photo_status_model.dart
@@ -4,6 +4,10 @@ class PhotoDisplay {
   final int? localId;
   final int? remoteId;
   final String? type;
+  final String path;
+  final String? url;
+  final PhotoStatusType status;
+  final String? errorMessage;
 
   PhotoDisplay({
     this.localId,
