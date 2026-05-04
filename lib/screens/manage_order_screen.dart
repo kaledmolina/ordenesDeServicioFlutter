792ccb82da9388d3e91e9f092e1623a0e83b583e
kaledmolina <kaledmoly@gmail.com>
2026-05-04 10:21:54 -0500

    feat: implement ManageOrderScreen with draft saving, image watermarking, and real-time deadline tracking

--- lib/screens/manage_order_screen.dart
+++ lib/screens/manage_order_screen.dart
@@ -5,6 +5,7 @@ import 'dart:typed_data';
 import 'package:flutter/services.dart';
 import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:path/path.dart' as p;
